c64dcbd1a7a51c3a1c5f786d1122f3b7f6ab7b9d
Nicolas Henry <nicolashenry50@mail.com>
2021-12-02 16:33:04 +0100

    New function for taxo subset

--- R/taxo-network.R
+++ R/taxo-network.R
@@ -1,24 +1,39 @@
+#' Generate data for taxonomic network
+#'
+#' @return A formatted table
+
+data_for_net <- function(){
+  require(data.table)
+  ranks <- c("kingdom","phylum","class","order","family","genus","species")
+  dat <- fread(here::here("data","laminaria_occurrance.csv.gz"))
+  dat[,species:=sub(" ","_",species)]
+  dat[,full_taxo:=paste(kingdom,phylum,class,order,family,genus,species,sep=";")]
+  dat[,full_taxo:=sub(";+$","",full_taxo)]
+
+  for(i in 2:length(ranks)){
+    dat[get(ranks[i])!="",(ranks[i]):=paste(get(ranks[i-1]),get(ranks[i]),sep=";")]
+    dat[get(ranks[i])=="",(ranks[i]):=""]
+  }
+  
+  dat
+}
+
 #' Generate a taxonomic network
 #'
 #' @return A taxonomic network
 
-taxo_network <- function(){
+taxo_network <- function(dat=data_for_net()){
   require(data.table)
   require(visNetwork, quietly = TRUE)
   
-  dat <- fread(here::here("data","laminaria_occurrance.csv.gz"))
-  dat[,species:=sub(" ","+",species)]
-  #dat_nodes[,full_taxo:=paste(kingdom,phylum,class,order,family,genus,species,sep="|")]
+  ranks <- c("kingdom","phylum","class","order","family","genus","species")
   
-  tmp <- c("kingdom","phylum","class","order","family","genus","species")
+  rank_taxo <- lapply(ranks,function(X){
+    data.table(rank=X,id=dat[get(X)!="",get(X)])
+  }) |> rbindlist()
   
-  for(i in 2:length(tmp)){
-    dat[get(tmp[i])!="",(tmp[i]):=paste(get(tmp[i-1]),get(tmp[i]),sep=";")]
-    dat[get(tmp[i])=="",(tmp[i]):=""]
-  }
-  
-  dat_edges <- lapply(1:(length(tmp)-1),function(i){
-    dat[,.SD,.SDcols=tmp[c(i,i+1)]] |> unique()
+  dat_edges <- lapply(1:(length(ranks)-1),function(i){
+    dat[,.SD,.SDcols=ranks[c(i,i+1)]] |> unique()
   }) |>
     rbindlist(use.names = FALSE) |>
     unique()
@@ -32,5 +47,28 @@ taxo_network <- function(){
   
   visNetwork(dat_nodes, dat_edges, width = "100%") |>
     visEdges(arrows = "to")
-  
 }
+
+#' Generate subseted table for focus
+#'
+#' @return A subseted table
+#' 
+#' 
+data_for_focus <- function(tax){
+  #tax <- "Chromista;Ochrophyta;Phaeophyceae;Laminariales;Laminariaceae;Laminaria;Laminaria_japonica"
+  
+  require(meowR)
+  require(data.table)
+  
+  last_rank_taxo <- sub("^.+;","",tax)
+  
+  dat <- data_for_net()
+  dat <- dat[grep(tax,full_taxo),.(eventDate,decimalLatitude,decimalLongitude)] |> 
+    na.omit() |>
+    data.table(label=last_rank_taxo)
+  
+  data.table(dat,getRegions(dat[,decimalLatitude], dat[,decimalLongitude]))
+}
+
+
+
