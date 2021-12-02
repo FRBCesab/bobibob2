883741fcf957914278a1cd36bf4408592ff89c71
Nicolas Henry <nicolashenry50@mail.com>
2021-12-02 17:22:43 +0100

    add render focus L japonica

--- make.R
+++ make.R
@@ -1,5 +1,7 @@
 source("R/taxo-network.R")
 
+rmarkdown::render(input = here::here("dashboard","focus","Laminaria_japonica.Rmd"))
+
 rmarkdown::render(input = here::here("dashboard","GBIF_dashboard.Rmd"),
                   output_file = here::here("index.html"))
 
