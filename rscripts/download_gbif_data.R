d4d79300e13e3de7389957ed6986e531752336bf
Anne Dubearnes <dubearna@tcd.ie>
2021-12-03 11:40:12 +0100

    Create occurrence.gz file

--- rscripts/download_gbif_data.R
+++ rscripts/download_gbif_data.R
@@ -6,7 +6,7 @@ gbif_mail <- Sys.getenv("GBIF_EMAIL")
 
 
 #Find taxon key corresponding to your organism of interest
-taxon_key <- find_taxon() #first argument : "taxon name", second argument "rank"
+taxon_key <- find_taxon("laminariales", "order") #first argument : "taxon name", second argument "rank"
 
 
 #Prepare occurence data dowload from Gbif
@@ -17,8 +17,8 @@ status<-gbif_download_init()
 check_status() # this step will run until STATUS = SUCCEEDED
 
 
-#Manually retreive Download key from the metadata called by the occ_download_meta() function
-dl_key <-""
+#Retreive Download key 
+dl_key <-"0071825-210914110416597" #enter the key manually from the metadata called by the occ_download_meta() function
 
 
 #Once download is ready (occ_download_meta$ = SUCCEEDED), fetch and import data
@@ -26,11 +26,15 @@ gbif_download(dl_key)
 
 
 #Unzip in data/occurrence folder and delete zip file from root (enter zip file name manually)
-unzip(zipfile = "0071182-210914110416597.zip", exdir = "./data/occurrence")
-unlink("0071182-210914110416597.zip")
+unzip(zipfile = "0071825-210914110416597.zip", exdir = "./data/occurrence")
+unlink("0071825-210914110416597.zip")
 #The file containing the occurrence data is "occurence.txt"
 
 
 #Create occurrence df
-occurrence <- data.table::fread("data/occurrence/fill/occurrence.txt")
+occurrence <- data.table::fread("data/occurrence/occurrence.txt")
+
+#Zip occurrence file to make it <100Mb (because of Github file size limitation) and move it up one level to /data
+R.utils::gzip("data/occurrence/occurrence.txt", destname = "data/occurrence.gz", remove = FALSE)
+
 
