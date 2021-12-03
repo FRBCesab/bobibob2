275a1404ccdd75d7a713143f05778adf6c81e1cc
Anne Dubearnes <dubearna@tcd.ie>
2021-12-03 13:31:45 +0100

    Add link to github repository and rename dashboard Help section, add \dontrun to examples in Roxygen headers

--- R/download-gbif.R
+++ R/download-gbif.R
@@ -4,8 +4,10 @@
 #' @param name The taxon name
 #' @param rank The taxon rank
 #' @return A taxon_key object containing the single identifier of the taxon in Gbif 
-#' @example
+#' @examples
+#' \dontrun{
 #' find_taxon ("laminaria digitata", "species")
+#' }
 #'
 find_taxon <- function (name, rank) {
   rgbif::name_suggest(q=name, rank=rank)$data$key[1]
@@ -15,8 +17,10 @@ find_taxon <- function (name, rank) {
 #' Prepare occurrence data download from Gbif
 #'
 #' @return Metadata about the download, including download status to check when the download is ready.
-#' @example
+#' @examples
+#' \dontrun{
 #' gbif_download_init()
+#' }
 #'
 gbif_download_init <- function () {
     res <- rgbif::occ_download(
@@ -33,8 +37,10 @@ gbif_download_init <- function () {
 #' Check status of download preparation
 #' 
 #' @return Messages indicating the status of the download prep (preparing, running, succedded), then metadata about the download request once the preparation has succeeded.
-#' @example
+#' @examples
+#' \dontrun{
 #' check_status()
+#' }
 check_status <- function(){
   rgbif::occ_download_wait(status)
   rgbif::occ_download_meta(status)
@@ -44,8 +50,10 @@ check_status <- function(){
 #' Download occurrence data from gbif
 #' @param dl_key The single identifier of the download prepared with the function gbif_download_init (The Download key printed by the function rgbif::occ_download_meta(res))
 #' @return a zip file containing the occurrence data imported from Gbif
-#' @example
+#' @examples
+#' \dontrun{
 #' gbif_download("0071104-210914110416597")
+#' }
 #'
 gbif_download <- function(dl_key) {
     dat <- rgbif::occ_download_get(dl_key)
